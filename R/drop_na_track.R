991488113aaad09b73cab6be7559afdbf80d7099
dmbwebb <dmbwebb@gmail.com>
2020-08-20 18:09:44 +0200

    added drop_na_count

---
+++ R/drop_na_track.R
@@ -0,0 +1,26 @@
+#' drop_na and see how many you drop
+#'
+#' @param data
+#' @param ... arguments to pass to drop_na
+#'
+#' @return
+#' @export
+#'
+#' @examples
+drop_na_count <- function(data, ...) {
+
+  dat_dropped <- drop_na(data, ...)
+
+  n_old <- nrow(data)
+  n_new <- nrow(dat_dropped)
+
+  tibble(
+    obs_type = c("kept", "removed", "total"),
+    n = c(n_new, n_old - n_new, n_old),
+    prop = n / n_old
+  ) %>%
+    print()
+
+  dat_dropped
+
+}
